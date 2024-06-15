78068430f7c4a6e48dd358a72387414f7de70d62
Vishal Balasaheb Khile <vbkhile192@gmail.com>
2024-06-15 15:17:48 +0530

    imported react draggable and export image library for text drag and for save img

--- MemeApp/vite-project/src/pages/Edit.jsx
+++ MemeApp/vite-project/src/pages/Edit.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { useState } from "react";
+import { useState , createRef } from "react";
 import Text from "../components/Text";
 import { useSearchParams } from "react-router-dom";
+import {exportComponentAsJPEG} from "react-component-export-image";
 
 const Editpage = () =>{
     const [params] = useSearchParams();
    const [count, setCount] = useState(0)
 
+   const memeRef = createRef();
+
     const addText = () => {setCount(count+1)}  
 
     return(
         <div>
-            <div>
+            <div ref={memeRef} className="meme mt-5 mb-5">
                 <img src={params.get("url")} width="250px" />
                 {
                     Array(count).fill(0).map(e => <Text />)
                 }
             </div>
             <button onClick={addText}>Edit</button>
+            <button variant = "success" onClick={(e) =>{}}>Save</button>
+           
         </div>
         
     )
